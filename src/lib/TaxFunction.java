000d2e1657176bb8ed7bd23384feb0af92581b2c
Glorious Satria Dhamang Aji <glorioussatria@gmail.com>
2024-04-23 08:59:48 +0700

    fix: create constant

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -1,28 +1,38 @@
-public static final int BASE_INCOME_TAX_THRESHOLD = 54000000;
-public static final int BASE_INSURANCE_THRESHOLD = 4500000;
-public static final int CHILD_ALLOWANCE_PER_CHILD = 1500000;
-public static final double TAX_RATE = 0.05;
+package lib;
 
-public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-    int tax = 0;
 
-    if (numberOfMonthWorking > 12) {
-        System.err.println("More than 12 months working per year");
-    }
+public class TaxFunction {
 
-    if (numberOfChildren > 3) {
-        numberOfChildren = 3;
-    }
+	public static final int BASE_INCOME_TAX_THRESHOLD = 54000000;
+	public static final int BASE_INSURANCE_THRESHOLD = 4500000;
+	public static final int CHILD_ALLOWANCE_PER_CHILD = 1500000;
+	public static final double TAX_RATE = 0.05;
 
-    if (isMarried) {
-        tax = (int) Math.round(TAX_RATE * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (BASE_INCOME_TAX_THRESHOLD + BASE_INSURANCE_THRESHOLD + (numberOfChildren * CHILD_ALLOWANCE_PER_CHILD))));
-    } else {
-        tax = (int) Math.round(TAX_RATE * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - BASE_INCOME_TAX_THRESHOLD));
-    }
+	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible,
+			boolean isMarried, int numberOfChildren) {
+		int tax = 0;
 
-    if (tax < 0) {
-        return 0;
-    } else {
-        return tax;
-    }
+		if (numberOfMonthWorking > 12) {
+			System.err.println("More than 12 months working per year");
+		}
+
+		if (numberOfChildren > 3) {
+			numberOfChildren = 3;
+		}
+
+		if (isMarried) {
+			tax = (int) Math.round(TAX_RATE * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking)
+					- deductible - (BASE_INCOME_TAX_THRESHOLD + BASE_INSURANCE_THRESHOLD
+							+ (numberOfChildren * CHILD_ALLOWANCE_PER_CHILD))));
+		} else {
+			tax = (int) Math.round(TAX_RATE * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking)
+					- deductible - BASE_INCOME_TAX_THRESHOLD));
+		}
+
+		if (tax < 0) {
+			return 0;
+		} else {
+			return tax;
+		}
+	}
 }
